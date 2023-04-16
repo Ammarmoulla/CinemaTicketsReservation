c6ce394ebcc8869b11d34fdb906d3ab3b1e9414e
Ammar moulla <ammarturing@gmail.com>
2023-04-16 13:14:14 +0300

    viewset and generic

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+import uuid
+
+router = DefaultRouter()
+router.register('guests', views.GuestViewset)
+router.register('movies', views.MovieViewset)
+router.register('reservations', views.ReservationViewset)
 
 urlpatterns = [
     #1
@@ -12,13 +19,33 @@ urlpatterns = [
     path('rest/fbv/', views.FBV_List),
 
     #3.2 GET PUT DELETE from rest framework function based view @api_view
-    path('rest/fbv/<int:pk>', views.FBV_pk),
+    path('rest/fbv/<uuid:pk>', views.FBV_pk),
 
     #4.1 GET POST from rest framework class based view APIView
-    #path('rest/cbv/', views.CBV_List.as_view()),
+    path('rest/cbv/', views.CBV_List.as_view()),
 
     #4.2 GET PUT DELETE from rest framework class based view APIView
-    #path('rest/cbv/<int:pk>', views.CBV_pk.as_view()),
+    path('rest/cbv/<uuid:pk>', views.CBV_pk.as_view()),
 
+    #5.1 GET POST from rest framework class based view mixins
+    path('rest/mixins/', views.mixins_list.as_view()),
+
+    #5.2 GET PUT DELETE from rest framework class based view mixins
+    path('rest/mixins/<uuid:pk>', views.mixins_pk.as_view()),
+
+    #6.1 GET POST from rest framework class based view generics
+    path('rest/generics/', views.generics_list.as_view()),
+
+    #6.2 GET PUT DELETE from rest framework class based view generics
+    path('rest/generics/<uuid:pk>', views.generics_pk.as_view()),
+    
+    #7 Viewsets
+    path('rest/viewsets/', include(router.urls)),
+
+    #8 find movie 
+    path('fbv/findmovie', views.find_movie),
+
+    #9 new reservation
+    path('fbv/newreservation',views.new_reservation),
 
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -4,8 +4,9 @@ from .models import Movie, Guest, Reservation
 from .serializers import MovieSerializer, GuestSerializer, ReservationSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework.views import APIView
+from rest_framework import generics, mixins, viewsets
 # Create your views here.
 
 
@@ -69,3 +70,121 @@ def FBV_pk(request, pk):
     elif request.method == "DELETE":
         guest.delete()
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    
+class CBV_List(APIView):
+    
+    def get(self, request):
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CBV_pk(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Guest, id=pk)
+    
+    def get(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        guest = self.get_object(pk)
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class mixins_list(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin, 
+    generics.GenericAPIView
+):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+class mixins_pk(
+    mixins.RetrieveModelMixin, 
+    mixins.UpdateModelMixin, 
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request)
+
+    def put(self, request, pk):
+        return self.update(request)
+    
+    def delete(self, request, pk):
+        return self.destroy(request)
+
+class generics_list(generics.ListCreateAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class generics_pk(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class GuestViewset(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+class MovieViewset(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [filters.SearchFilter]
+
+
+class ReservationViewset(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+@api_view(['GET'])
+def find_movie(request):
+    movie = Movie.objects.filter(
+        movie=request.data["movie"]
+    )
+    serializer = MovieSerializer(movie, many=True)  
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def new_reservation(request):
+    
+    guest = Guest.objects.create(
+            name=request.data["name"],
+            mobile=request.data["mobile"],
+        )
+    movie = Movie.objects.get(
+            movie=request.data["movie"],
+            hall=request.data["hall"]
+        )
+
+    reservation = Reservation.objects.create(
+            movie=movie,
+            guest=guest
+        )
+
+    return Response(status=status.HTTP_201_CREATED)
